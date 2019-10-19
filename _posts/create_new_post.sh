d4df9631e18f3be78feb2b5af9f80b4711555d50
eindiran <eindiran@uchicago.edu>
2019-10-19 00:33:18 -0700

    Support using getopts for argument handling in create_new_post.sh

--- _posts/create_new_post.sh
+++ _posts/create_new_post.sh
@@ -3,7 +3,8 @@
 #
 #          FILE: create_new_post.sh
 #
-#         USAGE: ./create_new_post.sh "<title>" [category 1, ..., category n]
+#         USAGE: ./create_new_post.sh [-l] -t "<title>" [-c "category 1 ... category n"]
+#                ./create_new_post.sh -h
 #
 #   DESCRIPTION: Creates a new titled & dated markdown file with the sketch
 #                of a new post.
@@ -14,20 +15,54 @@
 
 set -Eeuo pipefail
 
-POST_TITLE_RAW="$1"
-shift
-POST_CATEGORIES=$*
+LONG_FORM=false
+POST_TITLE_RAW=""
+CATEGORY_TAGS=""
+
+# Print the usage information and exit
+usage() {
+    printf "Usage: %s [-l] -t \"<title>\" [-c \"<tag 1> ... <tag n>\"]\n" "$0"
+    exit "$1"
+}
+
+# Handle the various arguments
+while getopts ":t:c:h:l" opt; do
+    case "${opt}" in
+        l) LONG_FORM=true           ;;
+        t) POST_TITLE_RAW=${OPTARG} ;;
+        c) CATEGORY_TAGS=${OPTARG}  ;;
+        h) usage 0                  ;;
+        *) usage 1                  ;;
+    esac
+done
+
+# Exit early if a title wasn't set
+if [ -z "$POST_TITLE_RAW" ]; then
+    usage 1
+fi
+
+# Add the category tag for short or long form posts
+#  * Long form  --> "article" tag
+#  * Short form --> "snip" tag
+if [ "$LONG_FORM" = true ]; then
+    CATEGORY_TAGS="${CATEGORY_TAGS} article"
+else
+    CATEGORY_TAGS="${CATEGORY_TAGS} snip"
+fi
+
+# Generate the filename
 POST_TITLE="$(echo "${POST_TITLE_RAW// /-}" | tr '[:upper:]' '[:lower:]')"
 POST_DATE="$(date -I)"
 POST_FILENAME="${POST_DATE}-${POST_TITLE}.markdown"
 
+# Create the file
 {
 cat <<- HEADER_EOF
 ---
 layout: post
 title:  "$POST_TITLE_RAW"
 date:   $POST_DATE
-categories: $POST_CATEGORIES
+categories: $CATEGORY_TAGS
 ---
 
 HEADER_EOF
